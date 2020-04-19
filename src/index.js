828562f3c2a4da44a0ca8b20e3d89a4c06aa3825
BRUL <jonathan.dequeker@gmail.com>
2020-04-19 14:33:50 +0200

    Prepared index for multiple pages
    added dom router
    lazy loading of pages
    router ga analytics
    snapshot renders

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
-
-import Info from './components/Info/Info'
-import Splash from './components/Splash/Splash'
+import React, { Suspense, lazy } from 'react';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router
+} from 'react-router-dom';
+import Analytics from 'react-router-ga';
+import { render } from 'react-snapshot';
 
 import './index.scss';
 
-ReactGA.initialize('UA-56321552-1'); // Here we should use our GA id
-ReactGA.pageview("/");
+const Info = lazy(() => import('./components/Info/Info'));
+const Splash = lazy(() => import('./components/Splash/Splash'));
 
-ReactDOM.render(
+render(
   <div className="App">
-    <Info />
-    <Splash />
+    <Router>
+      <Analytics id="UA-101955661-1" debug>
+        <Suspense fallback={<div className="Loading">Loading...</div>}>
+          <Route
+            render={({ location }) => (
+                <Switch location={location}>
+                  <Route exact path='/'>
+                    <Info />
+                    <Splash />
+                  </Route>
+                  <Route exact path='/landing' component={Landing} />
+                </Switch>
+            )}
+          />
+        </Suspense>
+      </Analytics>
+    </Router>
   </div>,
   document.getElementById('root')
 );
